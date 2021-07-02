0fdb845fdbbb276176f8a4f6b34a20c3ce25ca8b
JJFAO <jjfalonso@gmail.com>
2021-07-02 02:18:38 -0300

    Javascript: objetos y arrays de objetos.

--- Modulo2/JavaScript-Arrays/index.js
+++ Modulo2/JavaScript-Arrays/index.js
@@ -44,3 +44,98 @@ function ejercicioConArray() {
 
     console.log('La sumatoria de numeros pares es: ', total);
 }
+
+// Solicitar la cantidad de alumnos ✔
+// Por cada alumno ingresar una nota ✔
+// Mostrar la cantidad de alumnos aprobados, la nota aprobación 6. ✔
+// - Con una condición evaluar si la nota esta aprobada.
+// - Contar cada nota aprobada. Podemos usar una variable contador.
+
+function calcularAprobados() {
+    const cantidadAlumnos = parseInt(prompt('Ingrese la cantidad de alumnos 😒'));
+    const notas = [];
+
+    for (let i = 0; i < cantidadAlumnos; i++) {
+        const nota = prompt('ingrese la nota');
+        notas.push(nota);
+    }
+
+    let cantidadAprobados = 0;
+    for (let index = 0; index < notas.length; index++) {
+        const nota = notas[index];
+        if (nota >= 6) {
+            cantidadAprobados = cantidadAprobados + 1;
+        }
+    }
+
+    console.log(
+        'Del total de alumnos ' + cantidadAlumnos + ', la cantidad de aprobados es ⭐: ' + cantidadAprobados
+    );
+    console.log(`Del total de alumnos ${notas.length}, la cantidad de aprobados es 😎: ${cantidadAprobados}`);
+    // console.log('Las notas ingresadas son: ', notas)
+}
+
+const nombre = 'Ricardo';
+const rol = 'Co-mentor';
+
+const persona = {
+    nombre: 'Ricardo',
+    rol: 'Co-mentor',
+    apellido: 'Moreno',
+};
+
+const producto = {
+    'nombre-persona': 'cosa',
+    precio: 100,
+};
+
+const propiedad = 'apellido';
+
+// -----------------------
+// - "Hardcodear" un array de objetos de productos, con nombre y precio.
+// Pedir al usuario datos de nombre y precio,
+// para agregar un producto al array.
+
+const productos = [
+    {
+        nombre: 'Manzana 🍎',
+        precio: 100,
+    },
+    {
+        nombre: 'Naranja 🍊',
+        precio: 50,
+    },
+    {
+        nombre: 'Empanada 🥟',
+        precio: 50,
+    },
+];
+
+function agregarProductos() {
+    const nombre = prompt('Ingrese nombre de producto');
+    const precio = parseInt(prompt('Ingrese el precio del producto'));
+    const producto = { nombre: nombre, precio: precio };
+    productos.push(producto);
+    console.log(productos);
+}
+
+
+//-------- Adicional
+// Que el usuario pueda seleccionar distintos productos de la lista.
+ // - Listar productos para el usuario.
+ // - Seleccionar un producto del listado.
+function mostrarListado() {
+    // console.log(productos)
+    for (let i = 0; i < productos.length; i++) {
+        const producto = productos[i];
+        console.log(`codigo: ${i}, ${producto.nombre}, $${producto.precio}`)
+    }
+}
+
+
+
+
+// Que estos se agreguen a un array de compras.
+
+// Y al finalizar que muestre la suma total de todos los productos
+// que seleccionó el usuario.
