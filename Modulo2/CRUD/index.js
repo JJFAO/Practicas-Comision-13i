ca66e4e316f673fcd1804ddce6cca600a8f4c56c
JJFAO <jjfalonso@gmail.com>
2021-07-15 22:17:01 -0300

    CRUD: funcionalidad de eliminar y mostrar detalle de usuario Agregar datos de registro y id a usuarios nuevos

--- Modulo2/CRUD/index.js
+++ Modulo2/CRUD/index.js
@@ -3,8 +3,15 @@ const passInput = document.getElementById('pass');
 const nombreInput = document.getElementById('nombre');
 const rolInput = document.getElementById('rol');
 const contenidoTabla = document.getElementById('contenidoTabla');
+const detalleUsuarioDiv = document.getElementById('detalleUsuario');
 
-const usuarios = [];
+
+const usuarioJson = localStorage.getItem('usuarios');
+let usuarios = JSON.parse(usuarioJson) || [];
+
+function ID() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
 
 function agregarUsuario(event) {
     event.preventDefault();
@@ -18,23 +25,31 @@ function agregarUsuario(event) {
         pass: pass,
         nombre: nombre,
         rol: rol,
+        registro: Date.now(),
+        id: ID(),
     };
 
     usuarios.push(nuevoUsuario);
     const usuariosJson = JSON.stringify(usuarios);
     localStorage.setItem('usuarios', usuariosJson);
-
     event.target.reset();
     mostrarUsuarios();
 }
 
 function mostrarUsuarios() {
     function armarFilasDeUsuarios(usuario) {
+        // const fecha = new Date(usuario.registro)
+        // console.log(fecha.toLocaleString())
         const tr = `
         <tr>
             <td>${usuario.nombre}</td>
             <td>${usuario.rol}</td>
-            <td><button class="btn btn-warning">Editar</button></td>
+            <td>
+                <!-- Button trigger modal -->
+                <button onclick="mostrarDetalleUsuario('${usuario.id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDetalleUsuario"> Ver detalle </button>
+                <button class="btn btn-warning">Editar</button>
+                <button onclick="eliminarUsuario('${usuario.id}')" class="btn btn-danger">Eliminar</button>
+            </td>
         </tr>
         `;
         return tr;
@@ -43,3 +58,30 @@ function mostrarUsuarios() {
 
     contenidoTabla.innerHTML = contenido.join('');
 }
+
+function eliminarUsuario(id) {
+    function usuariosFilter(usuario) {
+        return usuario.id !== id;
+    }
+    const usuariosFiltrados = usuarios.filter(usuariosFilter);
+    usuarios = usuariosFiltrados;
+    mostrarUsuarios();
+    const usuariosJson = JSON.stringify(usuarios);
+    localStorage.setItem('usuarios', usuariosJson);
+}
+
+function mostrarDetalleUsuario(id) {
+    const usuarioEncontrado = usuarios.find(function (usuario) {
+        return usuario.id === id;
+    });
+
+    const contenido = `
+    <p>Nombre: ${usuarioEncontrado.nombre}</p>
+    <p>Correo: ${usuarioEncontrado.email}</p>
+    <p>Rol: ${usuarioEncontrado.rol}</p>
+    <p>Fecha de registro: ${usuarioEncontrado.registro}</p>
+    `;
+    detalleUsuarioDiv.innerHTML = contenido;
+}
+
+mostrarUsuarios();
