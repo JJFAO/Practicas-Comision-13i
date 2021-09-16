785749033584f9437b3d3958e9f4e85d21d38f87
rickywep <ricky.gnr.moreno@gmail.com>
2021-09-16 20:51:27 -0300

    configuracion inicial de nuestra api

---
+++ Modulo 4/api/routes/usuariosRoute.js
@@ -0,0 +1,10 @@
+// Rutas para crear usuarios
+const express = require('express');
+const router = express.Router();
+const usuarioController = require("../controllers/usuarioController");
+ 
+// Crear un usuario
+// api/usuarios
+router.post('/', usuarioController.crearUsuario);
+ 
+module.exports = router;
